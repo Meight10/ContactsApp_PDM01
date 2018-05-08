83a9f7b44a20b9511362cf0527aa873aad02d9dc
Meight <7ace.lex@gmail.com>
2018-05-08 01:29:39 -0600

    implementando métodos que estan en el fragment, dentro de eventos de click del icono de favorito

--- app/src/main/java/com/meight/contactsapp2/adapters/ContactsRvAdapter.java
+++ app/src/main/java/com/meight/contactsapp2/adapters/ContactsRvAdapter.java
@@ -6,9 +6,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.meight.contactsapp2.R;
+import com.meight.contactsapp2.fragments.FragmentContacts;
 import com.meight.contactsapp2.models.ContactModel;
 
 import java.util.ArrayList;
@@ -16,12 +18,18 @@ import java.util.ArrayList;
 public class ContactsRvAdapter extends RecyclerView.Adapter<ContactsRvAdapter.ViewHolderContacts> {
 
 
+    private final FragmentContacts.OnListFragmentInteractionListener interactionListener;
+    private final FragmentContacts.OnListFragmentInteractionIcon interactionIcon;
+
     private ViewPager viewPager;
 
     private ArrayList<ContactModel> listContacts;
 
-    public ContactsRvAdapter(ArrayList<ContactModel> listContacts){
+    public ContactsRvAdapter(ArrayList<ContactModel> listContacts, FragmentContacts.OnListFragmentInteractionListener interactionListener,
+    FragmentContacts.OnListFragmentInteractionIcon interactionIcon){
         this.listContacts= listContacts;
+        this.interactionListener = interactionListener;
+        this.interactionIcon = interactionIcon;
     }
 
     @NonNull
@@ -33,10 +41,33 @@ public class ContactsRvAdapter extends RecyclerView.Adapter<ContactsRvAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderContacts holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolderContacts holder, final int position) {
 
         holder.name.setText(listContacts.get(position).getName());
         holder.number.setText(listContacts.get(position).getNumber());
+
+        if (listContacts.get(position).getFavorite()) {
+            holder.favoriteIcon.setImageResource(R.drawable.ic_star_selected);
+        } else {
+            holder.favoriteIcon.setImageResource(R.drawable.ic_star_unselected);
+        }
+
+
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != interactionListener) {
+                    interactionListener.onListFragmentInteraction(listContacts.get(position));
+                }
+            }
+        });
+
+        holder.favoriteIcon.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                System.out.println("InteractionIconEvent" + position + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                interactionIcon.onListFragmentInteractionIcon(listContacts.get(position), position);
+            }
+        });
 
     }
 
@@ -48,12 +79,16 @@ public class ContactsRvAdapter extends RecyclerView.Adapter<ContactsRvAdapter.Vi
     public class ViewHolderContacts extends RecyclerView.ViewHolder {
 
         public final TextView name, number;
+        public final ImageView favoriteIcon;
+        public final View view;
 
         public ViewHolderContacts(View itemView) {
             super(itemView);
 
+            view = itemView;
             name = itemView.findViewById(R.id.name_txtV);
             number = itemView.findViewById(R.id.number_txtV);
+            favoriteIcon = itemView.findViewById(R.id.ic_favorites);
         }
     }
 }
